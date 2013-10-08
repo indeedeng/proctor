f8a10284b46d539307581af69da0a124e2630735
Charles Duffy <duffy@indeed.com>
2013-10-08 14:11:04 -0500

    PROC-109 close writer when done with generate-matrix mojo

--- proctor-maven-plugin/src/main/java/com/indeed/proctor/builder/maven/BuilderMojo.java
+++ proctor-maven-plugin/src/main/java/com/indeed/proctor/builder/maven/BuilderMojo.java
@@ -8,6 +8,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.Writer;
 
 @Mojo(name = "generate-matrix")
 public class BuilderMojo extends AbstractMojo {
@@ -30,7 +31,9 @@ public class BuilderMojo extends AbstractMojo {
             return;
         }
         try {
-            new LocalProctorBuilder(topDirectory, new FileWriter(outputFile), "".equals(author) ? author : null, version).execute();
+            Writer w = new FileWriter(outputFile);
+            new LocalProctorBuilder(topDirectory, w, "".equals(author) ? author : null, version).execute();
+            w.close();
         } catch (Exception e) {
             throw new MojoExecutionException("Failure during builder execution", e);
         }
