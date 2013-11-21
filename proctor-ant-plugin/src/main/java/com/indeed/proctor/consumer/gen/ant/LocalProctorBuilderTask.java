5fff528aaff7ffccd2961bb589a352101dfd1c43
Parker Seidel <parker@indeed.com>
2013-11-21 10:14:52 -0600

    PROC-118: Create ant task for building the test-matrix using argument conventions
    Attribute naming conventions from
    http://ant.apache.org/ant_task_guidelines.html
    destdir - output dir
    srcdir - input dir
    destfile - output file

---
+++ proctor-ant-plugin/src/main/java/com/indeed/proctor/consumer/gen/ant/LocalProctorBuilderTask.java
@@ -0,0 +1,92 @@
+package com.indeed.proctor.consumer.gen.ant;
+
+import com.indeed.proctor.builder.LocalProctorBuilder;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ * Executes the LocalProctorBuilder
+ * destdir  -   The output directory to write the matrix json file.
+ *              If set to '-', output is written to STDOUT
+ * destfile -   The name of the generated test-matrix JSON file.
+ * srcdir   -   The source directory containing the test-definitions directory
+ *              ${srcdir}/test-definitions
+ * author   -   Optional author string to be used for the test-matrix audit.author
+ * version  -   Optional version to be used for the test-matrix audit.version
+ * @author parker
+ */
+public class LocalProctorBuilderTask extends Task {
+    private String destdir;
+    private String destfile = "proctor-tests-matrix.json";
+    private String srcdir;
+
+    // Artifact overrides
+    private String author = null;
+    private long version = -1;
+
+    public String getDestdir() {
+        return destdir;
+    }
+
+    public void setDestdir(String destdir) {
+        this.destdir = destdir;
+    }
+
+    public String getSrcdir() {
+        return srcdir;
+    }
+
+    public void setSrcdir(String srcdir) {
+        this.srcdir = srcdir;
+    }
+
+    public String getDestfile() {
+        return destfile;
+    }
+
+    public void setDestfile(String destfile) {
+        this.destfile = destfile;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        try {
+            if(srcdir == null) {
+                throw new BuildException("srcdir is required");
+            }
+            if(destdir == null) {
+                throw new BuildException("destdir is required");
+            }
+            new LocalProctorBuilder(
+                    new File(srcdir),
+                    "-".equals(destdir) ?
+                        new PrintWriter(System.out) :
+                        new FileWriter(new File(destdir, destfile)),
+                    author,
+                    version).execute();
+        } catch (Exception e) {
+            throw new BuildException("Failed to create test matrix", e);
+        }
+
+    }
+}
