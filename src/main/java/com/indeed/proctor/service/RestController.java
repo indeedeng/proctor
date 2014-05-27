758dca88d1a85b35bbd9a2b919f9881b339ff28e
Piotr Kowalczyk <piotr@indeed.com>
2014-05-27 14:17:14 -0500

    Basic proctor loader stuff.
    I'm not sure if the spec is necessary.

--- src/main/java/com/indeed/proctor/service/RestController.java
+++ src/main/java/com/indeed/proctor/service/RestController.java
@@ -1,5 +1,9 @@
 package com.indeed.proctor.service;
 
+import com.indeed.proctor.common.AbstractProctorLoader;
+import com.indeed.proctor.common.Identifiers;
+import com.indeed.proctor.common.JsonProctorLoaderFactory;
+import com.indeed.proctor.common.Proctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +14,18 @@ import java.util.Map;
 
 @Controller
 public class RestController {
+
+    private final AbstractProctorLoader loader;
+
+    public RestController()
+    {
+        final JsonProctorLoaderFactory factory = new JsonProctorLoaderFactory();
+        factory.setFilePath("/var/lucene/proctor/proctor-tests-matrix.json");
+        factory.setSpecificationResource("/var/lucene/proctor/spec.json");
+        loader = factory.getLoader();
+        loader.load();
+    }
+
     @RequestMapping(value="/groups/identify", method= RequestMethod.GET)
     public @ResponseBody String groupsIdentify() {
         return "Hello, world!";
