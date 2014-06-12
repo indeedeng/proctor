77831f08b07fd94523d3a0297b25d8d6f86cc574
Piotr Kowalczyk <piotr@indeed.com>
2014-06-12 11:40:57 -0500

    Document PrefixVariable fields to explain purpose.

--- src/main/java/com/indeed/proctor/service/var/PrefixVariable.java
+++ src/main/java/com/indeed/proctor/service/var/PrefixVariable.java
@@ -8,10 +8,21 @@ import com.indeed.proctor.service.config.JsonVarConfig;
  * query parameters.
  */
 public abstract class PrefixVariable {
+    // The name used in Proctor rule expressions and as the key in the json service configuration.
     private final String varName;
+
+    // For ONLY query parameters, this string followed by a period is prefixed to all source keys for extraction.
+    // This lets us differentiate configured variables vs. built-in API parameters like "test".
     private final String prefix;
+
+    // Where the variable comes from in the HTTP request (QUERY or HEADER).
     private final ExtractorSource source;
+
+    // How the variable is referenced in the HTTP request. For example, user agents are typically User-Agent.
+    // This is the name we use to extract the variable from the HTTP request.
+    // If the configuration did not specify a sourceKey explicitly, we default it to varName.
     private final String sourceKey;
+
     private final ValueExtractor extractor;
 
     public PrefixVariable(final String varName, final JsonVarConfig varConfig, final String prefix) {
