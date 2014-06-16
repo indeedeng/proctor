6870b2f9f7333f1e8aff872af2bb5fa57d393179
Piotr Kowalczyk <piotr@indeed.com>
2014-06-16 18:29:32 -0500

    Add comment to BadRequestException.

--- proctor-service-core/src/main/java/com/indeed/proctor/service/core/web/BadRequestException.java
+++ proctor-service-core/src/main/java/com/indeed/proctor/service/core/web/BadRequestException.java
@@ -1,7 +1,7 @@
 package com.indeed.proctor.service.core.web;
 
 /**
- *
+ * Thrown when the user gives invalid request parameters to an API call.
  */
 public class BadRequestException extends RuntimeException {
     public BadRequestException(final String message) {
