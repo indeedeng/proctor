da7c88ad4c32b434a903a13093048eb8b3f47a2b
chrisc <chrisc@indeed.com>
2014-08-18 14:29:38 -0500

    PROW-151: Default value for a test definition should be "-1" +review CR @parker

--- proctor-common/src/main/java/com/indeed/proctor/common/model/TestDefinition.java
+++ proctor-common/src/main/java/com/indeed/proctor/common/model/TestDefinition.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * @author ketan
  */
 public class TestDefinition {
-    private String version;
+    private String version = "-1";
     @Nonnull
     private Map<String, Object> constants = Collections.emptyMap();
     @Nonnull
