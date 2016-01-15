51ec6568f01d515668cac0dac110be5f837d07cf
Antony Tran <atran@indeed.com>
2016-01-15 13:34:47 +0900

    PROW-159: Return invalid test error messages in ProctorLoadResult

--- proctor-common/src/main/java/com/indeed/proctor/common/ProctorLoadResult.java
+++ proctor-common/src/main/java/com/indeed/proctor/common/ProctorLoadResult.java
@@ -1,9 +1,13 @@
 package com.indeed.proctor.common;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
 
 import javax.annotation.Nonnull;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -11,32 +15,49 @@ import java.util.Set;
  */
 public class ProctorLoadResult {
     @Nonnull
-    private final Set<String> testsWithErrors;
+    private final Map<String, String> testErrorMap;
     @Nonnull
     private final Set<String> missingTests;
 
     private final boolean verifiedRules;
+
     public ProctorLoadResult(
             @Nonnull final Set<String> testsWithErrors,
             @Nonnull Set<String> missingTests
     ) {
-        this.testsWithErrors = testsWithErrors;
+        this.testErrorMap = makeTestErrorMap(testsWithErrors);
         this.missingTests = missingTests;
         this.verifiedRules = false;
     }
+
     public ProctorLoadResult(
             @Nonnull final Set<String> testsWithErrors,
             @Nonnull Set<String> missingTests,
             final boolean verifiedRules
     ) {
-        this.testsWithErrors = testsWithErrors;
+        this.testErrorMap = makeTestErrorMap(testsWithErrors);
+        this.missingTests = missingTests;
+        this.verifiedRules = verifiedRules;
+    }
+
+    public ProctorLoadResult(
+        @Nonnull final Map<String, String> testErrorMap,
+        @Nonnull Set<String> missingTests,
+        final boolean verifiedRules
+    ) {
+        this.testErrorMap = testErrorMap;
         this.missingTests = missingTests;
         this.verifiedRules = verifiedRules;
     }
 
     @Nonnull
     public Set<String> getTestsWithErrors() {
-        return testsWithErrors;
+        return testErrorMap.keySet();
+    }
+
+    @Nonnull
+    public Map<String, String> getTestErrorMap() {
+        return testErrorMap;
     }
 
     @Nonnull
@@ -44,13 +65,22 @@ public class ProctorLoadResult {
         return missingTests;
     }
 
-
     public boolean getVerifiedRules() {
         return verifiedRules;
     }
+
+    private static Map<String, String> makeTestErrorMap(final Set<String> testsWithErrors) {
+        return Maps.asMap(testsWithErrors, new Function<String, String>() {
+            @Override
+            public String apply(final String testName) {
+                return testName + " has an invalid specification";
+            }
+        });
+    }
+
     @SuppressWarnings("UnusedDeclaration")
     public boolean hasInvalidTests() {
-        return !(testsWithErrors.isEmpty() && missingTests.isEmpty());
+        return !(testErrorMap.isEmpty() && missingTests.isEmpty());
     }
 
     private static final ProctorLoadResult EMPTY = newBuilder().build();
@@ -65,14 +95,20 @@ public class ProctorLoadResult {
     }
 
     public static class Builder {
-        private ImmutableSet.Builder<String> testsWithErrors = ImmutableSet.builder();
+        private ImmutableMap.Builder<String, String> testsWithErrors = ImmutableMap.builder();
         private ImmutableSet.Builder<String> missingTests = ImmutableSet.builder();
         private boolean verifiedRules = false;
         private Builder() { }
 
         @Nonnull
+        public Builder recordError(final String testName, final String message) {
+            testsWithErrors.put(testName, message);
+            return this;
+        }
+
+        @Nonnull
         public Builder recordError(final String testName) {
-            testsWithErrors.add(testName);
+            testsWithErrors.put(testName, testName + " has an invalid specification");
             return this;
         }
 
