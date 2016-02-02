16b779c488115bbfdfdbf4d0a7712a29253a3c2b
Jeffrey Mathew <jmathew@indeed.com>
2016-02-02 11:22:26 -0600

    PROC-205: Include iso-8601 date in audit. +review srch @parker

--- proctor-common/src/main/java/com/indeed/proctor/common/model/Audit.java
+++ proctor-common/src/main/java/com/indeed/proctor/common/model/Audit.java
@@ -2,6 +2,10 @@ package com.indeed.proctor.common.model;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Contains basic metadata about a test matrix for auditing purposes
@@ -9,9 +13,12 @@ import javax.annotation.Nullable;
  */
 public class Audit {
     public static final String EMPTY_VERSION = "";
+    public static final TimeZone DEFAULT_TIMEZONE = TimeZone.getTimeZone("US/Central");
 
     private String version;
     private long updated;
+    @Nullable
+    private String updatedDate;
     @Nullable
     private String updatedBy;
 
@@ -29,6 +36,18 @@ public class Audit {
 
     public void setUpdated(final long updated) {
         this.updated = updated;
+        if (updated > 0) {
+            final DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mmZ");
+            formatter.setTimeZone(DEFAULT_TIMEZONE);
+            updatedDate = formatter.format(new Date(updated));
+        } else {
+            updatedDate = "";
+        }
+    }
+
+    @Nullable
+    public String getUpdatedDate() {
+        return updatedDate;
     }
 
     @Nullable
@@ -46,6 +65,7 @@ public class Audit {
         return "Audit{" +
                "version=" + version +
                ", updated=" + updated +
+               ", updatedDate=" + updatedDate +
                ", updatedBy='" + updatedBy + '\'' +
                '}';
     }
