5421bfe08a92e9ba0eabe99c41aba0316053649b
Antony Tran <atran@indeed.com>
2016-08-26 17:02:07 -0500

    PROC-235: Add a comment to explain the test

--- proctor-codegen-test/src/test/java/com/indeed/proctor/groups/TestUppercaseFirstCharacterGroups.java
+++ proctor-codegen-test/src/test/java/com/indeed/proctor/groups/TestUppercaseFirstCharacterGroups.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 import static com.indeed.proctor.groups.UppercaseFirstCharacterGroups.Test.UPPERCASE_FIRST_CHARACTER_TEST;
 
 /**
- *
+ * Test to ensure that Proctor tests that start with an uppercase character retain an unnormalized name in the
+ * generated groups class.
  */
 public class TestUppercaseFirstCharacterGroups {
 
