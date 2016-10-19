8d4d1c70a56941543ced92d96147be934b31fc1d
Yiqing Zhu <yiqing@indeed.com>
2016-10-19 14:46:29 +0900

    PROC-219: make cache holder an inner class of ProtorStoreCaching.

--- proctor-store/src/main/java/com/indeed/proctor/store/cache/ProctorStoreCaching.java
+++ proctor-store/src/main/java/com/indeed/proctor/store/cache/ProctorStoreCaching.java
@@ -1,6 +1,8 @@
 package com.indeed.proctor.store.cache;
 
 import com.google.common.base.Throwables;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.indeed.proctor.common.model.TestDefinition;
 import com.indeed.proctor.common.model.TestMatrixDefinition;
 import com.indeed.proctor.common.model.TestMatrixVersion;
@@ -9,10 +11,19 @@ import com.indeed.proctor.store.Revision;
 import com.indeed.proctor.store.StoreException;
 import org.apache.log4j.Logger;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Caching test histories
@@ -20,20 +31,22 @@ import java.util.Map;
 public class ProctorStoreCaching implements ProctorStore {
 
     private static final Logger LOGGER = Logger.getLogger(ProctorStoreCaching.class);
-
+    private static final long REFRESH_RATE_IN_SECOND = 10;
+    private static final long READ_TIMEOUT_IN_SECOND = 30;
+    private static final long WRITE_TIMEOUT_IN_SECOND = 180;
     private final ProctorStore delegate;
     private final CacheHolder cacheHolder;
 
     public ProctorStoreCaching(final ProctorStore delegate) {
         this.delegate = delegate;
-        cacheHolder = new CacheHolder(delegate);
+        cacheHolder = new CacheHolder();
         try {
             /**
              * We assume local repository is initialized, so we can call cacheHolder#start in constructor
              */
             cacheHolder.start();
         } catch (final StoreException e) {
-            LOGGER.error("Failed to initialize ProctorStoreCache", e);
+            LOGGER.error("Failed to initialize ProctorStoreCaching", e);
             Throwables.propagate(e);
         }
     }
@@ -187,5 +200,172 @@ public class ProctorStoreCaching implements ProctorStore {
     @Override
     public String getName() {
         return delegate.getName();
+    }
+
+    class CacheHolder {
+
+        private final AtomicReference<Map<String, List<Revision>>> historyCache = new AtomicReference<Map<String, List<Revision>>>();
+        private final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        private final Lock readLock = readWriteLock.readLock();
+        private final Lock writeLock = readWriteLock.writeLock();
+
+        private final AtomicReference<String> cachedLatestVersion = new AtomicReference<String>();
+        /* version information would changed so we don't expire */
+        private final Cache<String, TestMatrixVersion> versionCache = CacheBuilder.newBuilder()
+                .maximumSize(5)
+                .build();
+
+        private final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        private ScheduledFuture<?> scheduledFuture;
+        //private final ProctorStore proctorStore;
+
+        /*public CacheHolder(final ProctorStore proctorStore) {
+            delegate = proctorStore;
+        }*/
+
+        final Runnable updateCacheTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    tryUpdateCache();
+                } catch (final StoreException e) {
+                    LOGGER.error("Failed to update cache", e);
+                }
+            }
+        };
+
+        @Nonnull
+        public Map<String, List<Revision>> getCachedHistory() throws StoreException {
+            return synchronizedCacheRead(new Callable<Map<String, List<Revision>>>() {
+                @Override
+                public Map<String, List<Revision>> call() {
+                    return historyCache.get();
+                }
+            });
+        }
+
+        @Nonnull
+        public String getCachedLatestVersion() throws StoreException {
+            return synchronizedCacheRead(new Callable<String>() {
+                @Override
+                public String call() {
+                    return cachedLatestVersion.get();
+                }
+            });
+        }
+
+        public TestMatrixVersion getCachedTestMatrix(final String fetchVersion) throws StoreException {
+            return synchronizedCacheRead(new Callable<TestMatrixVersion>() {
+                @Override
+                public TestMatrixVersion call() throws StoreException {
+                    TestMatrixVersion testMatrix = versionCache.getIfPresent(fetchVersion);
+                    if (testMatrix == null) {
+                        testMatrix = delegate.getTestMatrix(fetchVersion);
+                        versionCache.put(fetchVersion, testMatrix);
+                    }
+                    return testMatrix;
+                }
+            });
+        }
+
+        public TestMatrixVersion getCachedCurrentTestMatrix() throws StoreException {
+            return synchronizedCacheRead(new Callable<TestMatrixVersion>() {
+                @Override
+                public TestMatrixVersion call() throws StoreException {
+                    final String latestVersion = getCachedLatestVersion();
+                    return getCachedTestMatrix(latestVersion);
+                }
+            });
+        }
+
+        private boolean hasNewVersion() throws StoreException {
+            final String newVersion = delegate.getLatestVersion();
+            return !newVersion.equals(getCachedLatestVersion());
+        }
+
+        protected void tryUpdateCache() throws StoreException {
+            delegate.refresh();
+            if (hasNewVersion()) {
+                lockAndUpdateCache();
+            } else {
+                LOGGER.info(String.format("[%s] Latest version not changed, no need to update cache", delegate.getName()));
+            }
+        }
+
+        private void lockAndUpdateCache() throws StoreException {
+            LOGGER.debug(String.format("[%s] Cache reload started", delegate.getName()));
+            synchronizedCacheWrite(new Callable<Void>() {
+                @Override
+                public Void call() throws StoreException {
+                    final TestMatrixVersion currentTestMatrix = delegate.getCurrentTestMatrix();
+                    final String newVersion = currentTestMatrix.getVersion();
+                    final Map<String, List<Revision>> allHistories = delegate.getAllHistories();
+                    versionCache.put(newVersion, currentTestMatrix);
+                    cachedLatestVersion.set(newVersion);
+                    historyCache.set(allHistories);
+                    return null;
+                }
+            });
+            LOGGER.debug(String.format("[%s] Cache reload finished", delegate.getName()));
+        }
+
+        public void start() throws StoreException {
+            LOGGER.info(String.format("[%s] Starting CacheHolder for branch ", delegate.getName()));
+            lockAndUpdateCache();
+            scheduledFuture = scheduledExecutorService
+                    .scheduleWithFixedDelay(updateCacheTask, REFRESH_RATE_IN_SECOND, REFRESH_RATE_IN_SECOND, TimeUnit.SECONDS);
+        }
+
+        public void reschedule() {
+            /**
+             * cancel scheduled task, executing task is allowed to finish;
+             */
+            LOGGER.info(String.format("[%s] Rescheduling UpdateCacheTask due to new updates.", delegate.getName()));
+            scheduledFuture.cancel(false);
+            try {
+                lockAndUpdateCache();
+            } catch (final StoreException e) {
+                LOGGER.error("failed to update the cache");
+            }
+
+            scheduledFuture = scheduledExecutorService
+                    .scheduleWithFixedDelay(updateCacheTask, REFRESH_RATE_IN_SECOND, REFRESH_RATE_IN_SECOND, TimeUnit.SECONDS);
+        }
+
+        private <T> T synchronizedCacheRead(final Callable<T> callable) throws StoreException {
+            try {
+                if (readLock.tryLock(READ_TIMEOUT_IN_SECOND, TimeUnit.SECONDS)) {
+                    try {
+                        return callable.call();
+                    } catch (final Exception e) {
+                        throw new StoreException("Failed perform read operation to read. ", e);
+                    } finally {
+                        readLock.unlock();
+                    }
+                } else {
+                    throw new StoreException("Cached is locked. Failed to acquire the lock. timeout. " + READ_TIMEOUT_IN_SECOND);
+                }
+            } catch (final InterruptedException e) {
+                throw new StoreException("Read operation to cache was interrupted", e);
+            }
+        }
+
+        private <T> T synchronizedCacheWrite(final Callable<T> callable) throws StoreException {
+            try {
+                if (writeLock.tryLock(WRITE_TIMEOUT_IN_SECOND, TimeUnit.SECONDS)) {
+                    try {
+                        return callable.call();
+                    } catch (final Exception e) {
+                        throw new StoreException.TestUpdateException("Failed perform write operation to cache. ", e);
+                    } finally {
+                        writeLock.unlock();
+                    }
+                } else {
+                    throw new StoreException.TestUpdateException("Failed to acquire the lock " + WRITE_TIMEOUT_IN_SECOND);
+                }
+            } catch (final InterruptedException e) {
+                throw new StoreException.TestUpdateException("Write operation to cache was interrupted", e);
+            }
+        }
     }
 }
--- proctor-store/src/test/java/com/indeed/proctor/store/cache/ProctorStoreCachingTest.java
+++ proctor-store/src/test/java/com/indeed/proctor/store/cache/ProctorStoreCachingTest.java
@@ -1,12 +1,12 @@
 package com.indeed.proctor.store.cache;
 
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -21,25 +21,21 @@ public class ProctorStoreCachingTest {
 
         List<Integer> result;
         result = ProctorStoreCaching.selectHistorySet(testee, 0, 1);
-        assertListEqual(Lists.newArrayList(1), result);
+        assertEquals(Lists.newArrayList(1), result);
 
         result = ProctorStoreCaching.selectHistorySet(testee, -1, 1);
-        assertListEqual(Lists.newArrayList(1), result);
+        assertEquals(Lists.newArrayList(1), result);
 
         result = ProctorStoreCaching.selectHistorySet(testee, 2, -1);
-        assertListEqual(Collections.<Integer>emptyList(), result);
+        assertEquals(Collections.<Integer>emptyList(), result);
 
         result = ProctorStoreCaching.selectHistorySet(testee, 0, Integer.MAX_VALUE);
-        assertListEqual(testee, result);
+        assertEquals(testee, result);
 
         result = ProctorStoreCaching.selectHistorySet(testee, 0, Integer.MIN_VALUE);
-        assertListEqual(Collections.<Integer>emptyList(), result);
+        assertEquals(Collections.<Integer>emptyList(), result);
 
         result = ProctorStoreCaching.selectHistorySet(testee, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        assertListEqual(testee, result);
-    }
-
-    private static void assertListEqual(final List<Integer> a, final List<Integer> b) {
-        Assert.assertArrayEquals(Iterables.toArray(a, Integer.class), Iterables.toArray(b, Integer.class));
+        assertEquals(testee, result);
     }
 }
