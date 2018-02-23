2580aee1a755c5e8bbb3069d479fc0de535b57de
Shoichi Nishimura <shoichi@indeed.com>
2018-02-23 11:24:08 +0900

    PROC-361: Add unit tests for test name filters

--- proctor-common/src/main/java/com/indeed/proctor/common/dynamic/TestNamePatternFilter.java
+++ proctor-common/src/main/java/com/indeed/proctor/common/dynamic/TestNamePatternFilter.java
@@ -1,14 +1,25 @@
 package com.indeed.proctor.common.dynamic;
 
 import com.indeed.proctor.common.model.ConsumableTestDefinition;
+import com.sun.javaws.exceptions.InvalidArgumentException;
 
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class TestNamePatternFilter implements DynamicFilter {
     private final Pattern pattern;
 
-    public TestNamePatternFilter(final String regexp) {
-        this.pattern = Pattern.compile(regexp);
+    /**
+     * Construct the filter from regular expression string.
+     * @param regex regular expression for test name pattern
+     * @throws IllegalArgumentException If the regular expression's syntax is invalid.
+     */
+    public TestNamePatternFilter(final String regex) {
+        try {
+            this.pattern = Pattern.compile(regex);
+        } catch (final PatternSyntaxException e) {
+            throw new IllegalArgumentException("the regular expression for test name pattern has syntax error.", e);
+        }
     }
 
     @Override
--- proctor-common/src/main/java/com/indeed/proctor/common/dynamic/TestNamePrefixFilter.java
+++ proctor-common/src/main/java/com/indeed/proctor/common/dynamic/TestNamePrefixFilter.java
@@ -1,11 +1,13 @@
 package com.indeed.proctor.common.dynamic;
 
+import com.google.common.base.Preconditions;
 import com.indeed.proctor.common.model.ConsumableTestDefinition;
 
 public class TestNamePrefixFilter implements DynamicFilter {
     private final String prefix;
 
     public TestNamePrefixFilter(final String prefix) {
+        Preconditions.checkArgument(!prefix.isEmpty(), "Prefix should be non-empty string");
         this.prefix = prefix;
     }
 
