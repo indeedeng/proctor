b2b6cf6002e788dc75efc4240763ea92aaebc344
Shoichi Nishimura <shoichi@indeed.com>
2018-02-23 11:27:25 +0900

    PROC-361: Add javadoc for TestNamePrefixFilter constructor

--- proctor-common/src/main/java/com/indeed/proctor/common/dynamic/TestNamePrefixFilter.java
+++ proctor-common/src/main/java/com/indeed/proctor/common/dynamic/TestNamePrefixFilter.java
@@ -6,6 +6,11 @@ import com.indeed.proctor.common.model.ConsumableTestDefinition;
 public class TestNamePrefixFilter implements DynamicFilter {
     private final String prefix;
 
+    /**
+     * Construct the filter from test name prefix string
+     * @param prefix prefix string matches the test names
+     * @throws IllegalArgumentException if prefix is empty string
+     */
     public TestNamePrefixFilter(final String prefix) {
         Preconditions.checkArgument(!prefix.isEmpty(), "Prefix should be non-empty string");
         this.prefix = prefix;
