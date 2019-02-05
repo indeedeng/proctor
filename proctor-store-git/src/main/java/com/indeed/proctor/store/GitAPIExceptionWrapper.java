5093c93dbe64e3a05ae749de4e7a6b33fe656d99
kisozuka <kisozuka@indeed.com>
2019-02-05 18:38:30 +0900

    PROW-771: Add test for GitAPIExceptionWrapper

--- proctor-store-git/src/main/java/com/indeed/proctor/store/GitAPIExceptionWrapper.java
+++ proctor-store-git/src/main/java/com/indeed/proctor/store/GitAPIExceptionWrapper.java
@@ -20,7 +20,7 @@ public class GitAPIExceptionWrapper {
                     return new GitNoDevelperAccessLevelException(
                             "Check if your access level is developer in [" + gitUrl.substring(0, gitUrl.length() - 4) + "/project_members]",
                             exception);
-                } else if (cause.getMessage().matches(".*50\\d.*")) {
+                } else if (cause.getMessage().matches("^50\\d\\s.*")) {
                     return new GitServerErrorException("Check if " + gitUrl + " is available", exception);
                 }
             } else if (cause instanceof IllegalStateException) {
