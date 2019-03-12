139f734f18eefd6b95f4ad54f745c21c54e19ff7
Natsuhiro Arita <natsuhiro@indeed.com>
2019-03-12 15:55:51 +0900

    PROW-808: Move the new test search API into a separate controller

--- proctor-webapp-library/src/main/java/com/indeed/proctor/webapp/controllers/ProctorController.java
+++ proctor-webapp-library/src/main/java/com/indeed/proctor/webapp/controllers/ProctorController.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Charsets;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.base.Joiner;
-import com.google.common.base.Splitter;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
@@ -22,7 +20,6 @@ import com.indeed.proctor.common.ProctorSpecification;
 import com.indeed.proctor.common.ProctorUtils;
 import com.indeed.proctor.common.Serializers;
 import com.indeed.proctor.common.TestSpecification;
-import com.indeed.proctor.common.model.Allocation;
 import com.indeed.proctor.common.model.ConsumableTestDefinition;
 import com.indeed.proctor.common.model.TestBucket;
 import com.indeed.proctor.common.model.TestDefinition;
@@ -38,18 +35,15 @@ import com.indeed.proctor.webapp.model.AppVersion;
 import com.indeed.proctor.webapp.model.RemoteSpecificationResult;
 import com.indeed.proctor.webapp.model.SessionViewModel;
 import com.indeed.proctor.webapp.model.WebappConfiguration;
-import com.indeed.proctor.webapp.util.TestSearchUtil;
 import com.indeed.proctor.webapp.util.threads.LogOnUncaughtExceptionHandler;
 import com.indeed.proctor.webapp.views.JsonView;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang.NotImplementedException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -59,7 +53,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -70,7 +63,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.function.BiFunction;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping({"/", "/proctor"})
@@ -144,157 +136,6 @@ public class ProctorController extends AbstractController {
         final TestMatrixVersion testMatrixVersion = getCurrentMatrix(which);
         final TestMatrixArtifact testMatrixArtifact = ProctorUtils.convertToConsumableArtifact(testMatrixVersion);
         return new JsonView(testMatrixArtifact);
-    }
-
-    private enum FilterType {
-        ALL,
-        TESTNAME,
-        DESCRIPTION,
-        RULE,
-        BUCKET,
-        BUCKETDESCRIPTION,
-    }
-
-    private enum FilterActive {
-        ALL,
-        ACTIVE,
-        INACTIVE,
-    }
-
-    private enum Sort {
-        FAVORITESFIRST,
-        TESTNAME,
-        UPDATEDDATE,
-    }
-
-    private static class TestsResponse {
-        private final List<TestNameAndDefinition> tests;
-        private final int totalTestCount;
-
-        TestsResponse(final List<TestNameAndDefinition> tests, final int totalTestCount) {
-            this.tests = tests;
-            this.totalTestCount = totalTestCount;
-        }
-
-        public List<TestNameAndDefinition> getTests() {
-            return tests;
-        }
-
-        public int getTotalTestCount() {
-            return totalTestCount;
-        }
-    }
-
-    private static class TestNameAndDefinition {
-        private final String name;
-        private final TestDefinition definition;
-
-        TestNameAndDefinition(final String name, final TestDefinition definition) {
-            this.name = name;
-            this.definition = definition;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public TestDefinition getDefinition() {
-            return definition;
-        }
-    }
-
-    private static boolean matchFilterType(
-            final String testName,
-            final TestDefinition definition,
-            final FilterType type,
-            final String query) {
-        if (query.isEmpty()) {
-            return true;
-        }
-        final String lowerQuery = query.toLowerCase();
-        switch (type) {
-            case TESTNAME:
-                return TestSearchUtil.matchTestName(testName, lowerQuery);
-            case DESCRIPTION:
-                return TestSearchUtil.matchDescription(definition, lowerQuery);
-            case RULE:
-                return TestSearchUtil.matchRule(definition, lowerQuery);
-            case BUCKET:
-                return TestSearchUtil.matchBucket(definition, lowerQuery);
-            case BUCKETDESCRIPTION:
-                return TestSearchUtil.matchBucketDescription(definition, lowerQuery);
-            case ALL:
-                return TestSearchUtil.matchAll(testName, definition, lowerQuery);
-            default:
-                throw new IllegalArgumentException("unknown filter type: " + type);
-        }
-    }
-
-    private static boolean matchFilterActive(final List<Allocation> allocations, final FilterActive filterActive) {
-        switch (filterActive) {
-            case ALL:
-                return true;
-            case ACTIVE:
-                return TestSearchUtil.matchActiveAllocation(allocations);
-            case INACTIVE:
-                return !TestSearchUtil.matchActiveAllocation(allocations);
-            default:
-                throw new IllegalArgumentException("unknown filter type: " + filterActive);
-        }
-    }
-
-    @VisibleForTesting
-    static Comparator<Entry<String, TestDefinition>> getComparator(final Sort sort, final Set<String> favoriteTestNames) {
-        switch(sort) {
-            case TESTNAME:
-                return Comparator.comparing(Entry::getKey, String::compareToIgnoreCase);
-            case FAVORITESFIRST:
-                return Comparator.comparing(
-                        Entry::getKey,
-                        TestSearchUtil.givenSetFirstComparator(favoriteTestNames)
-                                .thenComparing(String::compareToIgnoreCase));
-            case UPDATEDDATE:
-                throw new NotImplementedException("Updated date not implemented");
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
-
-    /**
-     * API for proctor tests with filtering functionality
-     *
-     * @param branch           environment
-     * @param limit            number of tests to return
-     * @param q                query string to search
-     * @param filterType       {@link FilterType}
-     * @param filterActive     {@link FilterActive}
-     * @param sort             {@link Sort}
-     * @param favoriteTestsRaw comma-separated favorite tests saved in cookie
-     * @return JSON of TestsResponse
-     */
-    @ApiOperation(value = "Proctor tests with filters", response = List.class)
-    @RequestMapping(value = "/matrix/tests", method = RequestMethod.GET)
-    public JsonView viewTestNames(
-            @RequestParam(defaultValue = "trunk") final String branch,
-            @RequestParam(defaultValue = "100") final int limit,
-            @RequestParam(defaultValue = "") final String q,
-            @RequestParam(defaultValue = "ALL") final FilterType filterType,
-            @RequestParam(defaultValue = "ALL") final FilterActive filterActive,
-            @RequestParam(defaultValue = "FAVORITESFIRST") final Sort sort,
-            @CookieValue(value = "FavoriteTests", defaultValue = "") final String favoriteTestsRaw) {
-        final Set<String> favoriteTestNames = Sets.newHashSet(Splitter.on(",").split(favoriteTestsRaw));
-
-        final Environment environment = determineEnvironmentFromParameter(branch);
-        final Map<String, TestDefinition> tests = getCurrentMatrix(environment).getTestMatrixDefinition().getTests();
-        final List<TestNameAndDefinition> result = tests.entrySet().stream()
-                    .filter(entry -> matchFilterType(entry.getKey(), entry.getValue(), filterType, q)
-                            && matchFilterActive(entry.getValue().getAllocations(), filterActive))
-                    .sorted(getComparator(sort, favoriteTestNames))
-                    .limit(limit)
-                    .map(entry -> new TestNameAndDefinition(entry.getKey(), entry.getValue()))
-                    .collect(Collectors.toList());
-
-        return new JsonView(new TestsResponse(result, tests.size()));
     }
 
     // not a @ApiOperation because it produces HTML
