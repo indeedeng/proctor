63a9cedf2ec1e461f5775deeb75e8103ec8b9ce3
Shoichi Nishimura <shoichi@indeed.com>
2019-05-15 14:37:59 +0900

    PROW-921: Ignore errors caused by client disconnect

--- proctor-webapp-library/src/main/java/com/indeed/proctor/webapp/views/JsonView.java
+++ proctor-webapp-library/src/main/java/com/indeed/proctor/webapp/views/JsonView.java
@@ -1,12 +1,15 @@
 package com.indeed.proctor.webapp.views;
 
-import com.indeed.proctor.common.Serializers;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.indeed.proctor.common.Serializers;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.View;
 
-import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author parker
@@ -14,11 +17,12 @@ import javax.servlet.http.HttpServletResponse;
 public class JsonView implements View {
 
     private static final String CONTENT_TYPE = "application/json;charset=utf-8";
+    private static final Logger LOGGER = Logger.getLogger(JsonView.class);
 
     private static final ObjectWriter JSON_WRITER = Serializers.strict().writerWithDefaultPrettyPrinter();
     private final Object data;
 
-    public JsonView(Object data) {
+    public JsonView(final Object data) {
         this.data = data;
     }
 
@@ -28,10 +32,27 @@ public class JsonView implements View {
     }
 
     @Override
-    public void render(Map<String, ?> model,
-                       HttpServletRequest request,
-                       HttpServletResponse response) throws Exception {
+    public void render(final Map<String, ?> model,
+                       final HttpServletRequest request,
+                       final HttpServletResponse response) throws Exception {
         response.setHeader("Content-Type", CONTENT_TYPE);
-        JSON_WRITER.writeValue(response.getWriter(), data);
+
+        try {
+            JSON_WRITER.writeValue(response.getWriter(), data);
+        } catch (final IOException e) {
+            if (isDisconnectedClientError(e)) {
+                LOGGER.warn("Client disconnected", e);
+                return;
+            }
+            throw e;
+        }
+    }
+
+    /**
+     * True if the throwable is caused because client disconnected during process
+     */
+    private static boolean isDisconnectedClientError(final Throwable e) {
+        final Throwable cause = ExceptionUtils.getRootCause(e);
+        return cause instanceof IOException && "broken pipe".equalsIgnoreCase(cause.getMessage());
     }
 }
