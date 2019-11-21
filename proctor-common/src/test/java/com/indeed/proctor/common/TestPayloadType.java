63538242f0a7750845a40fb8b64f80d257dfc167
Thibault Kruse <tkruse@indeed.com>
2019-11-21 17:23:11 +0900

    NOBUG: Modernize Payload tests

--- proctor-common/src/test/java/com/indeed/proctor/common/TestPayloadType.java
+++ proctor-common/src/test/java/com/indeed/proctor/common/TestPayloadType.java
@@ -1,131 +1,125 @@
 package com.indeed.proctor.common;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.indeed.proctor.common.model.Payload;
-import com.indeed.proctor.common.PayloadType;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
+import static com.indeed.proctor.common.PayloadType.payloadTypeForValue;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-/**
- * @author pwp
- */
 public class TestPayloadType {
     @Test
     public void testPayloadTypeNames() {
-        List<String> names = PayloadType.allTypeNames();
+        final List<String> names = PayloadType.allTypeNames();
         assertEquals(7, names.size());
 
-        for (String s : new String[]{"doubleValue", "doubleArray",
-                                     "longValue", "longArray",
-                                     "stringValue", "stringArray","map"}) {
-            PayloadType p = PayloadType.payloadTypeForName(s);
+        for (final String s : new String[]{
+                "doubleValue", "doubleArray",
+                "longValue", "longArray",
+                "stringValue", "stringArray",
+                "map"
+        }) {
+            final PayloadType p = PayloadType.payloadTypeForName(s);
             assertEquals(s, p.payloadTypeName);
         }
         // Verify that trying to get the PayloadType for a nonsense
         // name throws an exception.
-        try {
-            PayloadType p = PayloadType.payloadTypeForName("gruntleBuggy");
-            assertFalse("should have thrown IllegalArgumentException on \"gruntleBuggy\"", true);
-        } catch (IllegalArgumentException e) {
-            // expected.
-        }
-    }
-
-    private Payload[] getTestPayloads() {
-        Payload[] payloads = new Payload[7];
-        for (int i = 0; i < payloads.length; i++)
-            payloads[i] = new Payload();
-        payloads[0].setDoubleValue(47.0D);
-        payloads[1].setDoubleArray(new Double[]{98.0D, -137D});
-        payloads[2].setLongValue(42L);
-        payloads[3].setLongArray(new Long[]{9L, 8L, 7L, 6L, 5L, 4L});
-        payloads[4].setStringValue("foobar");
-        payloads[5].setStringArray(new String[]{"foo", "bar", "baz"});
-        payloads[6].setMap(ImmutableMap.of("a", new Double(1.0D), "b", new Double[]{57.0D, -8.0D, 79.97D}, "c", (Object) "somevals"));
-        return payloads;
-    }
-
-    // Must be in the same order as getTestPayloads().
-    private PayloadType[] allPayloadTypes() {
-        return new PayloadType[]{PayloadType.DOUBLE_VALUE, PayloadType.DOUBLE_ARRAY,
-                                  PayloadType.LONG_VALUE, PayloadType.LONG_ARRAY,
-                                  PayloadType.STRING_VALUE, PayloadType.STRING_ARRAY,
-                                  PayloadType.MAP};
+        assertThatThrownBy(() -> PayloadType.payloadTypeForName("gruntleBuggy"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void testPayloadTypeForValueRetrieval_DoubleArrayType() {
-        assertEquals(PayloadType.DOUBLE_ARRAY,PayloadType.payloadTypeForValue(new ArrayList<Double>(){{add(1.1D);}}));
-        assertFalse(PayloadType.DOUBLE_ARRAY.equals(PayloadType.payloadTypeForValue(new ArrayList<Long>(){{add(100L);}})));
-        assertEquals(PayloadType.DOUBLE_ARRAY,PayloadType.payloadTypeForValue(new ArrayList<Float>(){{add(1.2F);}}));
-        assertEquals(PayloadType.DOUBLE_ARRAY,PayloadType.payloadTypeForValue(new Double[]{1.0D, 2.0D}));
-        assertEquals(PayloadType.DOUBLE_ARRAY,PayloadType.payloadTypeForValue(new Float[]{1.0F, 2.0F}));
-        assertFalse(PayloadType.DOUBLE_ARRAY.equals(PayloadType.payloadTypeForValue(new Long[]{100L, 200L})));
+        assertEquals(PayloadType.DOUBLE_ARRAY, payloadTypeForValue(singletonList(1.1D)));
+        assertNotEquals(PayloadType.DOUBLE_ARRAY, payloadTypeForValue(singletonList(100L)));
+        assertEquals(PayloadType.DOUBLE_ARRAY, payloadTypeForValue(singletonList(1.2F)));
+        assertEquals(PayloadType.DOUBLE_ARRAY, payloadTypeForValue(new Double[]{1.0D, 2.0D}));
+        assertEquals(PayloadType.DOUBLE_ARRAY, payloadTypeForValue(new Float[]{1.0F, 2.0F}));
+        assertNotEquals(PayloadType.DOUBLE_ARRAY, payloadTypeForValue(new Long[]{100L, 200L}));
     }
 
     @Test
     public void testPayloadTypeForValueRetrieval_LongArrayType() {
-        assertEquals(PayloadType.LONG_ARRAY,PayloadType.payloadTypeForValue(new ArrayList<Long>(){{add(11L);}}));
-        assertEquals(PayloadType.LONG_ARRAY,PayloadType.payloadTypeForValue(new ArrayList<Integer>(){{add(11);}}));
-        assertFalse(PayloadType.LONG_ARRAY.equals(PayloadType.payloadTypeForValue(new ArrayList<Double>(){{add(1.1D);}})));
-        assertEquals(PayloadType.LONG_ARRAY,PayloadType.payloadTypeForValue(new Long[]{100L, 200L}));
-        assertEquals(PayloadType.LONG_ARRAY,PayloadType.payloadTypeForValue(new Integer[]{10, 20}));
-        assertFalse(PayloadType.LONG_ARRAY.equals(PayloadType.payloadTypeForValue(new Double[]{1.0D, 2.0D})));
+        assertEquals(PayloadType.LONG_ARRAY, payloadTypeForValue(singletonList(11L)));
+        assertEquals(PayloadType.LONG_ARRAY, payloadTypeForValue(singletonList(11)));
+        assertNotEquals(PayloadType.LONG_ARRAY, payloadTypeForValue(singletonList(1.1D)));
+        assertEquals(PayloadType.LONG_ARRAY, payloadTypeForValue(new Long[]{100L, 200L}));
+        assertEquals(PayloadType.LONG_ARRAY, payloadTypeForValue(new Integer[]{10, 20}));
+        assertNotEquals(PayloadType.LONG_ARRAY, payloadTypeForValue(new Double[]{1.0D, 2.0D}));
     }
 
     @Test
     public void testPayloadTypeForValueRetrieval_StringArrayType() {
-        assertEquals(PayloadType.STRING_ARRAY,PayloadType.payloadTypeForValue(new ArrayList<String>(){{add("Ya");}}));
-        assertFalse(PayloadType.STRING_ARRAY.equals(PayloadType.payloadTypeForValue(new ArrayList<Integer>(){{add(100);}})));
-        assertEquals(PayloadType.STRING_ARRAY,PayloadType.payloadTypeForValue(new String[]{"yea", "Ya"}));
-        assertFalse(PayloadType.STRING_ARRAY.equals(PayloadType.payloadTypeForValue(new Float[]{1.0F, 2.0F})));
+        assertEquals(PayloadType.STRING_ARRAY, payloadTypeForValue(singletonList("Ya")));
+        assertNotEquals(PayloadType.STRING_ARRAY, payloadTypeForValue(singletonList(100)));
+        assertEquals(PayloadType.STRING_ARRAY, payloadTypeForValue(new String[]{"yea", "Ya"}));
+        assertNotEquals(PayloadType.STRING_ARRAY, payloadTypeForValue(new Float[]{1.0F, 2.0F}));
     }
 
     @Test
     public void testPayloadTypeForValueRetrieval_NonArrayTypes() {
-        assertEquals(PayloadType.MAP,PayloadType.payloadTypeForValue(ImmutableMap.of("string","string")));
-        assertEquals(PayloadType.LONG_VALUE,PayloadType.payloadTypeForValue(100));
-        assertEquals(PayloadType.LONG_VALUE,PayloadType.payloadTypeForValue(100L));
-        assertEquals(PayloadType.DOUBLE_VALUE,PayloadType.payloadTypeForValue(1.1D));
-        assertEquals(PayloadType.DOUBLE_VALUE,PayloadType.payloadTypeForValue(2.1F));
-        assertEquals(PayloadType.STRING_VALUE,PayloadType.payloadTypeForValue("yes"));
+        assertEquals(PayloadType.MAP, payloadTypeForValue(ImmutableMap.of("string", "string")));
+        assertEquals(PayloadType.LONG_VALUE, payloadTypeForValue(100));
+        assertEquals(PayloadType.LONG_VALUE, payloadTypeForValue(100L));
+        assertEquals(PayloadType.DOUBLE_VALUE, payloadTypeForValue(1.1D));
+        assertEquals(PayloadType.DOUBLE_VALUE, payloadTypeForValue(2.1F));
+        assertEquals(PayloadType.STRING_VALUE, payloadTypeForValue("yes"));
     }
 
     @Test
     public void testUnknownPayloadTypeForValue() {
-        try {
-            PayloadType.payloadTypeForValue(new ArrayList());
-        } catch(IllegalArgumentException e) {
-            //expected
-        }
-        try {
-            PayloadType.payloadTypeForValue(PayloadType.DOUBLE_VALUE);
-        } catch(IllegalArgumentException e) {
-            //expected
-        }
+        assertThatThrownBy(() -> payloadTypeForValue(emptyList()))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> payloadTypeForValue(PayloadType.DOUBLE_VALUE))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void testPayloadHasType() {
-        final Payload[] payloads = getTestPayloads();
-        final PayloadType[] payloadTypes = allPayloadTypes();
-        for (int i = 0; i < payloads.length; i++) {
-            for (int j = 0; j < payloadTypes.length; j++) {
-                if (i == j) {
-                    assertTrue("should be true: payloadTypes["+j+"].payloadHasThisType(payloads["+i+"])", payloadTypes[j].payloadHasThisType(payloads[i]));
-                    assertTrue("should be true: payloads[i].fetchType().equals(payloadTypes[j].payloadTypeName",
-                            payloads[i].fetchType().equals(payloadTypes[j].payloadTypeName));
+        final Map<PayloadType, Payload> inputs = getPayloadTypePayloadSampleMap();
+
+        inputs.forEach((type, payload) -> {
+            inputs.keySet().forEach(type2 -> {
+                if (type == type2) {
+                    assertTrue(payload + " should have type " + type2, type2.payloadHasThisType(payload));
+                    assertEquals(payload.fetchType(), type2.payloadTypeName);
                 } else {
-                    assertFalse("should be false: payloadTypes["+j+"].payloadHasThisType(payloads["+i+"])", payloadTypes[j].payloadHasThisType(payloads[i]));
+                    assertFalse(payload + " should not have type " + type2, type2.payloadHasThisType(payload));
                 }
-            }
-        }
+            });
+        });
     }
+
+    private static Map<PayloadType, Payload> getPayloadTypePayloadSampleMap() {
+        final ImmutableList<Consumer<Payload>> map = ImmutableList.<Consumer<Payload>>builder()
+                .add(p -> p.setDoubleValue(47.0D))
+                .add(p -> p.setDoubleArray(new Double[]{98.0D, -137D}))
+                .add(p -> p.setLongValue(42L))
+                .add(p -> p.setLongArray(new Long[]{9L, 8L, 7L, 6L, 5L, 4L}))
+                .add(p -> p.setStringValue("foobar"))
+                .add(p -> p.setStringArray(new String[]{"foo", "bar", "baz"}))
+                .add(p -> p.setMap(ImmutableMap.of("a", 1.0D, "b", new Double[]{57.0D, -8.0D, 79.97D}, "c", "somevals")))
+                .build();
+        return map.stream()
+                .map(consumer -> {
+                    final Payload p = new Payload();
+                    consumer.accept(p);
+                    return p;
+                })
+                .collect(Collectors.toMap(
+                        e -> PayloadType.payloadTypeForName(e.fetchType()), e -> e));
+    }
+
 }
