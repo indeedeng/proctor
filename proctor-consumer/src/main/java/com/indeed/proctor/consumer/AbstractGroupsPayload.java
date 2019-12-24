7288819e420f83d69d6e1c28e71e2fe71ce7828f
Thibault Kruse <tkruse@indeed.com>
2019-12-24 18:13:03 +0900

    NOBUG: Refactor: extract common converters

--- proctor-consumer/src/main/java/com/indeed/proctor/consumer/AbstractGroupsPayload.java
+++ proctor-consumer/src/main/java/com/indeed/proctor/consumer/AbstractGroupsPayload.java
@@ -10,6 +10,15 @@ import java.util.function.Function;
 
 public abstract class AbstractGroupsPayload {
 
+    /**
+     * Used because ObjectMapper does not always read in longs as longs (can be integer)
+     */
+    private static final Function<Object, Long> LONG_CONVERTER = o -> ((Number) o).longValue();
+    /**
+     * Used because ObjectMapper does not always read in doubles as doubles (can be integer)
+     */
+    private static final Function<Object, Double> DOUBLE_CONVERTER = o -> ((Number) o).doubleValue();
+
     @Nullable
     protected String convertToStringValue(final Payload payload, final String payloadMapKey) throws IllegalArgumentException {
         checkPayloadExist(payload, payloadMapKey);
@@ -19,33 +28,33 @@ public abstract class AbstractGroupsPayload {
 
     protected Long convertToLongValue(final Payload payload, final String payloadMapKey) throws IllegalArgumentException {
         checkPayloadExist(payload, payloadMapKey);
-        return extractNonNullValueFromMapPayload(payload, payloadMapKey, Number.class).longValue();
+        return extractNonNullValueFromMapPayload(payload, payloadMapKey, LONG_CONVERTER);
     }
 
     protected Double convertToDoubleValue(final Payload payload, final String payloadMapKey) throws IllegalArgumentException {
         checkPayloadExist(payload, payloadMapKey);
-        return extractNonNullValueFromMapPayload(payload, payloadMapKey, Number.class).doubleValue();
+        return extractNonNullValueFromMapPayload(payload, payloadMapKey, DOUBLE_CONVERTER);
     }
 
     @SuppressWarnings("unchecked")
     protected String[] convertToStringArray(final Payload payload, final String payloadMapKey) throws IllegalArgumentException {
         checkPayloadExist(payload, payloadMapKey);
-        final List<Object> list = extractNonNullValueFromMapPayload(payload, payloadMapKey, List.class);
+        final List<Object> list = extractNonNullValueFromMapPayload(payload, payloadMapKey, o -> (List) o);
         return convertToTypedArray(list, o -> (String) o, String.class);
     }
 
     @SuppressWarnings("unchecked")
     protected Long[] convertToLongArray(final Payload payload, final String payloadMapKey) throws IllegalArgumentException {
         checkPayloadExist(payload, payloadMapKey);
-        final List<Object> list = extractNonNullValueFromMapPayload(payload, payloadMapKey, List.class);
-        return convertToTypedArray(list, i -> ((Number) i).longValue(), Long.class);
+        final List<Object> list = extractNonNullValueFromMapPayload(payload, payloadMapKey, o -> (List) o);
+        return convertToTypedArray(list, LONG_CONVERTER, Long.class);
     }
 
     @SuppressWarnings("unchecked")
     protected Double[] convertToDoubleArray(final Payload payload, final String payloadMapKey) throws IllegalArgumentException {
         checkPayloadExist(payload, payloadMapKey);
-        final List<Object> list = extractNonNullValueFromMapPayload(payload, payloadMapKey, List.class);
-        return convertToTypedArray(list, i -> ((Number) i).doubleValue(), Double.class);
+        final List<Object> list = extractNonNullValueFromMapPayload(payload, payloadMapKey, o -> (List) o);
+        return convertToTypedArray(list, DOUBLE_CONVERTER, Double.class);
     }
 
     private static void checkPayloadExist(final Payload payload, final String payloadMapKey) {
@@ -65,10 +74,10 @@ public abstract class AbstractGroupsPayload {
     private static <T> T extractNonNullValueFromMapPayload(
             final Payload payload,
             final String payloadMapKey,
-            final Class<T> clazz
+            final Function<Object, T> converter
     ) {
         // assumes (from checkPayloadExist) that payload != null, payload.getMap() != null, ...
-        final T result = (T) payload.getMap().get(payloadMapKey);
+        final T result = converter.apply(payload.getMap().get(payloadMapKey));
         if (result != null) {
             return result;
         }
