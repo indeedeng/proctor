04243533e962fe307c1990f1964ba2b4f269bb8d
Syd Polk <syd@indeed.com>
2020-06-05 15:58:18 -0500

    PROC-817 Add TestMatrixArtifactProctorLoader

---
+++ proctor-common/src/main/java/com/indeed/proctor/common/TestMatrixArtifactProctorLoader.java
@@ -0,0 +1,49 @@
+package com.indeed.proctor.common;
+
+import com.indeed.proctor.common.model.TestMatrixArtifact;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.el.FunctionMapper;
+import java.io.IOException;
+
+/**
+ * Support using an already-loaded test matrix with an arbitrary specification.
+ * @author syd
+ */
+
+public class TestMatrixArtifactProctorLoader extends AbstractProctorLoader {
+    @Nonnull
+    private final String source;
+    @Nonnull
+    private final TestMatrixArtifact testMatrixArtifact;
+
+    public TestMatrixArtifactProctorLoader(
+            @Nonnull ProctorSpecification proctorSpecification,
+            @Nonnull final String source,
+            @Nonnull final TestMatrixArtifact testMatrixArtifact,
+            @Nonnull final FunctionMapper functionMapper) {
+        super(TestMatrixArtifactProctorLoader.class, proctorSpecification, functionMapper);
+        this.source = source;
+        this.testMatrixArtifact = testMatrixArtifact;
+    }
+
+    public TestMatrixArtifactProctorLoader(
+            @Nonnull ProctorSpecification proctorSpecification,
+            @Nonnull final String source,
+            @Nonnull final TestMatrixArtifact testMatrixArtifact) {
+        this(proctorSpecification, source, testMatrixArtifact, RuleEvaluator.FUNCTION_MAPPER);
+    }
+
+    @CheckForNull
+    @Override
+    protected TestMatrixArtifact loadTestMatrix() throws IOException, MissingTestMatrixException {
+        return testMatrixArtifact;
+    }
+
+    @Nonnull
+    @Override
+    protected String getSource() {
+        return source;
+    }
+}
