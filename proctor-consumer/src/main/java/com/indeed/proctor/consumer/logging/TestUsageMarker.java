18c5f5b8ac4d4fe88492ac36a478f5f2f3b506ed
Thibault Kruse <tkruse@indeed.com>
2020-12-03 14:31:19 +0900

    PROC-865: Simplify TestUsageMarker

--- proctor-consumer/src/main/java/com/indeed/proctor/consumer/logging/TestUsageMarker.java
+++ proctor-consumer/src/main/java/com/indeed/proctor/consumer/logging/TestUsageMarker.java
@@ -1,44 +1,31 @@
 package com.indeed.proctor.consumer.logging;
 
+import com.google.common.collect.MapMaker;
 import com.indeed.proctor.common.ProctorResult;
 
-import javax.annotation.concurrent.ThreadSafe;
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * Helper class to mark testNames when used.
  */
-@ThreadSafe
 public class TestUsageMarker {
 
-    /**
-     * Using AtomicBoolean Map to stay thread-safe without synchronization penalty.
-     * Thread-safety is only relevant for unusual multi-threaded usages of this class.
-     */
-    private final Map<String, AtomicBoolean> testMarkers;
+    private final Set<String> testMarkers;
 
     public TestUsageMarker(final ProctorResult proctorResult) {
-        testMarkers = proctorResult.getBuckets().keySet().stream()
-                .collect(Collectors.toMap(k -> k, k -> new AtomicBoolean(false)));
+        // using MapMaker to get the equivalent of ConcurrentHashMap with less thread-contention
+        testMarkers = Collections.newSetFromMap(new MapMaker()
+                .initialCapacity(proctorResult.getBuckets().size())
+                .makeMap());
     }
 
     public void markTests(final Collection<String> testNames) {
-        for (final String testName: testNames) {
-            final AtomicBoolean marker = testMarkers.get(testName);
-            if (marker != null) {
-                marker.set(true);
-            }
-        }
+        testMarkers.addAll(testNames);
     }
 
     public boolean isMarked(final String testName) {
-        final AtomicBoolean atomicBoolean = testMarkers.get(testName);
-        if (atomicBoolean == null) {
-            return false;
-        }
-        return atomicBoolean.get();
+        return testMarkers.contains(testName);
     }
 }
