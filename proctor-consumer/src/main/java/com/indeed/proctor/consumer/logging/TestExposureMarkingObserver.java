6a406898a6a368507f7699ed0ec6ab14f6e54a18
Thibault Kruse <tkruse@indeed.com>
2020-12-04 10:38:59 +0900

    NOBUG: Refactor: Use plain ConcurrentHashMap

--- proctor-consumer/src/main/java/com/indeed/proctor/consumer/logging/TestExposureMarkingObserver.java
+++ proctor-consumer/src/main/java/com/indeed/proctor/consumer/logging/TestExposureMarkingObserver.java
@@ -5,29 +5,33 @@ import com.google.common.collect.Sets;
 import com.indeed.proctor.common.ProctorResult;
 
 import java.util.Collection;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Observer that marks all used tests and provides the collected tests as ProctorResult
  */
 public class TestExposureMarkingObserver implements TestUsageObserver {
 
-    private final TestUsageMarker testUsageMarker;
+    private final Set<String> testMarkers;
 
     private final ProctorResult originalResult;
 
     public TestExposureMarkingObserver(final ProctorResult originalResult) {
         this.originalResult = originalResult;
-        testUsageMarker = new TestUsageMarker(originalResult.getBuckets().size());
+        // Use ConcurrentHashMap to avoid ConcurrentModificationExceptions without locking on retrieval
+        // the typical access should not be multithreaded anyway, this is just caution for unusual usage
+        testMarkers = ConcurrentHashMap.newKeySet(originalResult.getBuckets().size());
     }
 
     @Override
     public void markTestsUsed(final Collection<String> testNames) {
-        testUsageMarker.markTests(testNames);
+        testMarkers.addAll(testNames);
     }
 
     @Override
     public void markTestUsed(final String testName) {
-        testUsageMarker.markTest(testName);
+        testMarkers.add(testName);
     }
 
     /**
@@ -37,9 +41,9 @@ public class TestExposureMarkingObserver implements TestUsageObserver {
         return new ProctorResult(
                 originalResult.getMatrixVersion(),
                 // using Guava views to prevent object creation overhead
-                Maps.filterKeys(originalResult.getBuckets(), testUsageMarker::isMarked),
-                Maps.filterKeys(originalResult.getAllocations(), testUsageMarker::isMarked),
-                Maps.filterKeys(originalResult.getTestDefinitions(), testUsageMarker::isMarked),
-                Sets.filter(originalResult.getDynamicallyLoadedTests(), testUsageMarker::isMarked));
+                Maps.filterKeys(originalResult.getBuckets(), testMarkers::contains),
+                Maps.filterKeys(originalResult.getAllocations(), testMarkers::contains),
+                Maps.filterKeys(originalResult.getTestDefinitions(), testMarkers::contains),
+                Sets.filter(originalResult.getDynamicallyLoadedTests(), testMarkers::contains));
     }
 }
