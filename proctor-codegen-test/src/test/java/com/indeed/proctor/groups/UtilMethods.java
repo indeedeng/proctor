423a25c4f0cc6e86634c0287d462a541ad4ec44b
Zack Goodwin <zgoodwin@indeed.com>
2023-07-06 11:17:57 -0400

    PROC-960: Remove maven publish blocks and style fixes

--- proctor-codegen-test/src/test/java/com/indeed/proctor/groups/UtilMethods.java
+++ proctor-codegen-test/src/test/java/com/indeed/proctor/groups/UtilMethods.java
@@ -17,7 +17,6 @@ import java.io.Reader;
 import java.io.StringWriter;
 import java.util.Map;
 
-import static jdk.internal.loader.BootLoader.findResource;
 import static org.junit.Assert.assertTrue;
 
 public class UtilMethods {
--- symlinkproctor.sh
+++ symlinkproctor.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-mkdir  ~/.gradle/local-repo/maven/com
-mkdir  ~/.gradle/local-repo/maven/com/indeed
+mkdir -p ~/.gradle/local-repo/maven/com/indeed
 ln -s ~/.m2/repository/com/indeed/proctor*  ~/.gradle/local-repo/maven/com/indeed || echo "Link already exists"
