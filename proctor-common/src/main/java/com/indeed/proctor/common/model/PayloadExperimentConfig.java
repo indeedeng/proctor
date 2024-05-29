ab37af2b33945e887d15f44f3ef4ed2a33b0095c
Zack Goodwin <zgoodwin@indeed.com>
2024-05-29 11:26:36 -0400

    PROC-1528: Add payload experiment config fields

--- proctor-common/src/main/java/com/indeed/proctor/common/model/PayloadExperimentConfig.java
+++ proctor-common/src/main/java/com/indeed/proctor/common/model/PayloadExperimentConfig.java
@@ -1,6 +1,5 @@
 package com.indeed.proctor.common.model;
 
-import lombok.Builder;
 import lombok.Data;
 
 import javax.annotation.Nonnull;
@@ -8,7 +7,6 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 @Data
-@Builder
 public class PayloadExperimentConfig {
     @Nullable String priority;
     @Nonnull List<String> namespace;
